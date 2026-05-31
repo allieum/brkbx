ae6b86a9fcee548f080b1fafa766cd4b9caaf78f
lyric <56244147+allieum@users.noreply.github.com>
2026-05-31 11:21:29 -0700

    copy_samples.py

---
+++ scripts/copy_samples.py
@@ -0,0 +1,24 @@
+import os
+import machine
+
+os.mount(machine.SDCard(1), '/sd')
+
+for d in ('/flash/samples', '/flash/samples/160'):
+    try:
+        os.mkdir(d)
+    except OSError:
+        pass  # already exists
+
+src_dir = '/sd/samples/160'
+dst_dir = '/flash/samples/160'
+files = sorted(f for f in os.listdir(src_dir) if f.endswith('.wav'))
+print(f'copying {len(files)} files to internal flash...')
+
+CHUNK = 4096
+for f in files:
+    print(f'  {f}')
+    with open(f'{src_dir}/{f}', 'rb') as src, open(f'{dst_dir}/{f}', 'wb') as dst:
+        while buf := src.read(CHUNK):
+            dst.write(buf)
+
+print('done')
